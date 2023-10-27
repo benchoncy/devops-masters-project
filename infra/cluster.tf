dd564bc880db7233e92cc051e9f93d7577c6e790
Ben Stuart <ben@benstuart.ie>
2023-10-27 17:58:55 +0100

    Increase node count

--- infra/cluster.tf
+++ infra/cluster.tf
@@ -57,9 +57,9 @@ resource "aws_eks_node_group" "main" {
   instance_types = ["t3.medium"]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 3
-    min_size     = 1
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
   }
 
   update_config {
