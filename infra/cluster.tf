2a3a1f7aade4aa567609b176cedc0ea8d7abdb8c
Ben Stuart <ben@benstuart.ie>
2023-11-11 19:57:41 +0000

    Scale cluster

--- infra/cluster.tf
+++ infra/cluster.tf
@@ -57,9 +57,9 @@ resource "aws_eks_node_group" "main" {
   instance_types = ["m5.xlarge"]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
   }
 
   update_config {
