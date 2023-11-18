904ca92a505645cf71853e9536459cdc67dd9159
Ben Stuart <ben@benstuart.ie>
2023-11-18 20:16:12 +0000

    Add sagemaker training image

--- src/pipelines/workloads/training/sagemaker.py
+++ src/pipelines/workloads/training/sagemaker.py
@@ -2,9 +2,8 @@ import sagemaker
 import boto3
 import argparse
 from sagemaker.workflow.pipeline import Pipeline
-from sagemaker.workflow.steps import ProcessingStep, TrainingStep
-from sagemaker.processing import ScriptProcessor
-from sagemaker.pytorch.estimator.pytorch import PyTorch
+from sagemaker.workflow.steps import TrainingStep
+from sagemaker.pytorch.estimator import PyTorch
 from src.pipelines.instrumentation import create_marker
 
 experiment_id = "training"
@@ -12,28 +11,28 @@ account_id = boto3.client("sts").get_caller_identity()["Account"]
 role = f"arn:aws:iam::{account_id}:role/sm-workflow"
 region = boto3.session.Session().region_name
 sagemaker_session = sagemaker.session.Session()
-sagemaker_image = f"{account_id}.dkr.ecr.{region}.amazonaws.com/benchoncy-devops-masters-project/sagemaker:latest"
+sagemaker_image = f"{account_id}.dkr.ecr.{region}.amazonaws.com/benchoncy-devops-masters-project/sagemaker-training:latest"
 
 
 def make_step(name, step, depends_on=[], run_id=0):
     estimator = PyTorch(
+        image_uri=sagemaker_image,
         entry_point=f"src/pipelines/workloads/{experiment_id}/step_{step}.py",
         source_dir=".",
         role=role,
-        framework_version="2.0.1",
-        py_version="py310",
         instance_count=1,
         instance_type="ml.m5.large",  # 2 vCPU + 8 GiB
         volume_size=30,
         sagemaker_session=sagemaker_session,
         hyperparameters={
-            "run-id": run_id,
+            "run-id": str(run_id),
         },
     )
     return TrainingStep(
         name=name,
         estimator=estimator,
-        depends_on=depends_on
+        depends_on=depends_on,
+        inputs={},  # No inputs
     )
 
 
